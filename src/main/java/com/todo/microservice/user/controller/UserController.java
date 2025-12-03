b1e7548aa8a3959a7ddeb580e8ffd430d0c41e06
tzcontainers <tznana@yahoo.com>
2025-12-03 17:55:04 +0000

    removed git yml files from main

--- src/main/java/com/todo/microservice/user/controller/UserController.java
+++ src/main/java/com/todo/microservice/user/controller/UserController.java
@@ -3,20 +3,23 @@ package com.todo.microservice.user.controller;
 import com.todo.microservice.user.model.User;
 import com.todo.microservice.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
     UserService userService;
 
     @PostMapping("/signUp")
-    public User signUp(@RequestBody User user) throws Exception {
-        return userService.addUser(user);
+    public ResponseEntity<User> signUp(@RequestBody User user) throws Exception {
+        User addUser = userService.addUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }
